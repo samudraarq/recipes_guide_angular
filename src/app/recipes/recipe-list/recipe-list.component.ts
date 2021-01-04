688ac661963cbbdfdca3f8eaf245724b4babba93
Samudra Faris Arqam <samudrafaris@gmail.com>
2021-01-04 20:39:38 +0700

    fix bootstrap styling

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -13,6 +13,11 @@ export class RecipeListComponent implements OnInit {
       'This is a test recipe',
       'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=960,872'
     ),
+    new Recipe(
+      'First Recipe',
+      'This is a test recipe',
+      'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=960,872'
+    ),
   ];
 
   constructor() {}
